33ba1e3a578c69dd5859deedf1b10f52fd67f892
Angad Singh <mail@angad.dev>
2021-11-16 10:57:21 +0530

    updates

--- lua/my/plugins.lua
+++ lua/my/plugins.lua
@@ -88,6 +88,13 @@ return require('packer').startup(function()
   use 'jiangmiao/auto-pairs'
   use 'gregsexton/MatchTag'
 
+  -- NVIM tree
+  use {
+    'kyazdani42/nvim-tree.lua',
+    requires = 'kyazdani42/nvim-web-devicons',
+    config = function() require'nvim-tree'.setup {} end
+  }
+
   -- PHP/Laravel
   use 'jwalton512/vim-blade'
 
